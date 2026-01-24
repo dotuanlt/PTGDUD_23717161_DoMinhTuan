7195cb511a18280fb3f5db3ea218230ad572e48e
dotuanlt <dotuan2004.gl@gmail.com>
2026-01-24 21:59:44 +0700

    Tuan 2 React Fundamentals Bai 1

--- Tuan2/React_JS/src/App.jsx
+++ Tuan2/React_JS/src/App.jsx
@@ -4,30 +4,43 @@ import Alert from './component/Alert'
 import { useState } from 'react'
 import LoginForm from './component/LoginForm'
 import Responsive from './component/Responsive'
+import Header from './component/Header'
+import StudentInfo from './component/StudentInfo'
+import Footer from './component/Footer'
 
 function App() {
-  const [bgColor, setBgColor] = useState('#4d83d2')
+  // const [bgColor, setBgColor] = useState('#4d83d2')
 
   return(
-    //Bài 2:
+    //Tuần 1: Bài 2:
     // <>
     //   <Button type="primary">Primary</Button>
     //   <Button type="success">Success</Button>
     //   <Button type="danger">Danger</Button>
     // </>
 
-    //Bài 3:
+    //Tuần 1: Bài 3:
     // <div style = {{backgroundColor: bgColor, height: '600px', width: '500px'}}>
     //   <Alert title = "Success" style = "success" onClick = {() => setBgColor('lightgreen')}></Alert>
     //   <Alert title = "Warning" style = "warning" onClick = {() => setBgColor('rgb(236, 240, 128)')}></Alert> 
     //   <Alert title = "Error" style = "error" onClick = {() => setBgColor('#f15b5b')}></Alert> 
     // </div>
 
-    //Bài 4:
+    //Tuần 1: Bài 4:
     // <LoginForm></LoginForm>
 
-    //Bài 5:
-    <Responsive></Responsive>
+    //Tuần 1: Bài 5:
+    // <Responsive></Responsive>
+
+    //Tuần 2: Bài 1
+    <>
+      <Header name = 'Đỗ Minh Tuấn'></Header>
+      <StudentInfo name = 'Đỗ Minh Tuấn' mssv = '23717161' className = 'DHKTPM19BTT'>
+
+      </StudentInfo>
+      <Footer name = 'Đỗ Minh Tuấn'></Footer>
+    </>
+    
   )
   
 }
---
+++ Tuan2/React_JS/src/component/StudentInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function StudentInfo(props) {
+  return (
+    <div style={{height: '400px', width: '100%', fontSize: '20px', margin: 'auto', color: '#000', fontWeight: 'bold'}}>
+        <table style={{width: '50%', margin: 'auto', }}>
+            <tr>
+                <td colspan="2">THÔNG TIN SINH VIÊN</td>
+            </tr>
+
+            <tr>
+                <td>Tên sinh viên: </td>
+                <td>{props.name}</td>
+            </tr>
+
+            <tr>
+                <td>MSSV: </td>
+                <td>{props.mssv}</td>
+            </tr>
+
+            <tr>
+                <td>Lớp: </td>
+                <td>{props.className}</td>
+            </tr>
+
+        </table>
+    </div>
+  )
+}
+
+export default StudentInfo
