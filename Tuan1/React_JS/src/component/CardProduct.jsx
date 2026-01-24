2476480cf0dc8eca2dcbfe60a6f5e9c8ef92728d
dotuanlt <dotuan2004.gl@gmail.com>
2026-01-24 08:37:07 +0700

    Tuan 1 ReactJS Bai 1

---
+++ Tuan1/React_JS/src/component/CardProduct.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CardProduct.css'
+
+function CardProduct() {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD71CN_xowBXY5vRDff_dchd2NCRZH2sG-bw&s" alt="" />
+      <h2>Áo thun</h2>
+      <p>100.000 đ</p>
+      <button>Add to cart</button>
+    </div>
+  )
+}
+
+export default CardProduct
