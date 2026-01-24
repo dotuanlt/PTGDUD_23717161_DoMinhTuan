95eb1f19b17345c391a317b0f2170a20de2af34f
dotuanlt <dotuan2004.gl@gmail.com>
2026-01-24 08:52:56 +0700

    Tuan 2 React Bai 2

---
+++ Tuan2/React_JS/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './component/Button.jsx'
+
+function App() {
+  return(
+    <>
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="danger">Danger</Button>
+    </>
+  )
+  
+}
+
+export default App
