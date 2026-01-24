9ec018b59a38635f7b1e500e16595c455177dfbc
dotuanlt <dotuan2004.gl@gmail.com>
2026-01-24 18:10:41 +0700

    Tuan 2 React Bai 4

--- Tuan2/React_JS/src/App.jsx
+++ Tuan2/React_JS/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 // import Button from './component/Button.jsx'
 import Alert from './component/Alert'
 import { useState } from 'react'
+import LoginForm from './component/LoginForm'
 
 function App() {
   const [bgColor, setBgColor] = useState('#4d83d2')
@@ -15,11 +16,14 @@ function App() {
     // </>
 
     //Bài 3:
-    <div style = {{backgroundColor: bgColor, height: '600px', width: '500px'}}>
-      <Alert title = "Success" style = "success" onClick = {() => setBgColor('lightgreen')}></Alert>
-      <Alert title = "Warning" style = "warning" onClick = {() => setBgColor('rgb(236, 240, 128)')}></Alert> 
-      <Alert title = "Error" style = "error" onClick = {() => setBgColor('#f15b5b')}></Alert> 
-    </div>
+    // <div style = {{backgroundColor: bgColor, height: '600px', width: '500px'}}>
+    //   <Alert title = "Success" style = "success" onClick = {() => setBgColor('lightgreen')}></Alert>
+    //   <Alert title = "Warning" style = "warning" onClick = {() => setBgColor('rgb(236, 240, 128)')}></Alert> 
+    //   <Alert title = "Error" style = "error" onClick = {() => setBgColor('#f15b5b')}></Alert> 
+    // </div>
+
+    //Bài 4:
+    <LoginForm></LoginForm>
 
   )
   
