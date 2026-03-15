4b0d3a8bcb56671c03a2c3107f7e82548ecddb4e
dotuanlt <dotuan2004.gl@gmail.com>
2026-03-15 15:26:48 +0700

    Tuan 4 Bai 3

--- Tuan4/buoi4_FetchAPI/src/App.jsx
+++ Tuan4/buoi4_FetchAPI/src/App.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [user, setUser] = useState(null);
 
   //Bài 1:
   // useEffect(() => {
@@ -22,41 +24,54 @@ function App() {
   // }, [])
 
   //Bài 2:
-  useEffect(() => {
-    var url = "https://jsonplaceholder.typicode.com/users";
-    
-    async function fetchdata () {
-      var res = await fetch(url);
+  // useEffect(() => {
+  //   var url = "https://jsonplaceholder.typicode.com/users";
 
-      try {
-          if(res.ok){
-            var data = await res.json();
-            setUsers(data);
-            setTimeout(() => {
-              setLoading(false);
-            }, 1000);
-            console.log("succes");
-          }
-          else{
-            console.log("error");  
-          }
-          
-      } catch (error) {
-        console.log("error")
+  //   async function fetchdata () {
+  //     var res = await fetch(url);
+
+  //     try {
+  //         if(res.ok){
+  //           var data = await res.json();
+  //           setUsers(data);
+  //           setTimeout(() => {
+  //             setLoading(false);
+  //           }, 1000);
+  //           console.log("succes");
+  //         }
+  //         else{
+  //           console.log("error");
+  //         }
+
+  //     } catch (error) {
+  //       console.log("error")
+  //     }
+  //   }
+  //   fetchdata();
+
+  // }, [])
+
+  // Bài 3:
+  async function findUser() {
+    var url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    var res = await fetch(url);
+
+    try {
+      if (res.ok) {
+        var data = await res.json();
+        setUser(data);
+        console.log(data);
+      } else {
+        console.log("User not found");
       }
-
-
-      
-      
+    } catch (error) {
+      console.log("error");
     }
-    fetchdata();
-  
-  }, [])
-  
+  }
 
   return (
     <>
-    {/* Bài 1: */}
+      {/* Bài 1: */}
       {/* {
         users.map((item) => {
           return <div key = {item.id}>
@@ -66,9 +81,8 @@ function App() {
         })
       } */}
 
-
-    {/* Bài 2: */}
-    {
+      {/* Bài 2: */}
+      {/* {
       loading ? (<h2>Loading...</h2>): 
           (users.map((item) => {
             return <div key = {item.id}>
@@ -76,11 +90,44 @@ function App() {
               <h2>{item.email}</h2>
             </div>
         }))
-    }
-    
+    } */}
 
+      {/* Bài 3: */}
+      {
+        <div>
+          <span>Nhập ID của User cần tìm: </span>
+          <input
+            type="text"
+            name="get_UserID"
+            id="get_UserID"
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <input
+            style={{ margin: "10px" }}
+            type="button"
+            value="Find"
+            onClick={findUser}
+          />
+          <table border={1}>
+            <tbody>
+              <tr>
+                <td style={{ width: "100px" }}>Name</td>
+                <td style={{ width: "200px" }}>{user?.name}</td>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <td>{user?.phone}</td>
+              </tr>
+              <tr>
+                <td>Website</td>
+                <td>{user?.website}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
